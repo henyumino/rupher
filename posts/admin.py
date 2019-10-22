b08b8a2757703065f8be9c46f66de6e6314b160b
henyumino <henyumino37@gmail.com>
2019-10-22 14:37:05 +0800

    add inline comment

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Comment
 
-admin.site.register(Post)
+class CommentInline(admin.StackedInline):
+    model = Comment
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline, ]
+
+admin.site.register(Post, PostAdmin)
+
+
