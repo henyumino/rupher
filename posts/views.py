1da54f52a368dbfe1eee3b878dd15fa69032d9c5
henyumino <henyumino37@gmail.com>
2019-10-22 12:57:48 +0800

    add admin,layout,static file

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 from .models import Post
 
 def index(request):
-    # posts = Post.objects.all()
-    # output= ', '.join([str(post) for post in posts])
-    # return HttpResponse(output)
-    return render(request,'posts/index.html')
+    posts = Post.objects.all()
+    return render(request,'posts/index.html',{'posts':posts})
