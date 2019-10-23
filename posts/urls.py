d8a4d56b44b04e020609c3649920e3b72d773c69
henyumino <henyumino37@gmail.com>
2019-10-23 15:42:06 +0800

    imporve comment system

--- posts/urls.py
+++ posts/urls.py
@@ -8,6 +8,8 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index,name='index'),
     path('<slug:slug>', views.single, name='single'),
+    path('comment/<int:id>', views.comment,name='comment'),
+    path('comment-edit/<int:id>', views.comment_edit,name='comment-edit'),
 ]
 
 hander404 = 'views.handler404'
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponse
-from .models import Post
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post,Comment
 
 def handler404(request):
     return render(request, '404.html', status=404)
@@ -12,3 +12,28 @@ def index(request):
 def single(request, slug):
     post = get_object_or_404(Post,slug = slug)
     return render(request,'posts/single.html',{'post':post})
+
+def comment(request,id):
+    post = get_object_or_404(Post, pk=id)
+
+    if request.method == 'POST':
+        newDesc = request.POST['desc']
+
+        if len(newDesc) < 10:
+            return render(request, 'posts/single.html',{
+                                'post':post,
+                                'errors':'minimal 10 karakter'
+                                })
+
+        post.comment_set.create(desc=newDesc,user_id='1')
+        return HttpResponseRedirect('/posts/%s'%post.slug) 
+
+def comment_edit(request,id):
+    comment = get_object_or_404(Comment, pk=id)
+
+    if request.method == 'POST':
+        newDesc = request.POST['desc']
+        Comment.objects.filter(pk=id).update(desc=newDesc)
+        return HttpResponseRedirect('/posts')
+    
+    return render(request, 'posts/comment_edit.html',{'comment':comment})
